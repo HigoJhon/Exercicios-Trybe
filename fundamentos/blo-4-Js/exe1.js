c42d13a30b9227178ce6a834b08e9a3f54be66a8
Higo Jhon <higojhon98@gmail.com>
2022-08-18 21:30:00 -0300

    Primeiro exer

---
+++ fundamentos/blo-4-Js/exe1.js
@@ -0,0 +1,10 @@
+const myName = "Higo Jhon"
+
+const birthCity ="São luis"
+//birthCity = "Rio De Janeiro"
+let birthYear = 1998
+birthYear =2030
+
+console.log (myName)
+console.log(birthYear)
+console.log(birthCity)
