1754101c3fb3a06b09d962faa232f998ea9240de
Higo Jhon <higojhon98@gmail.com>
2022-08-23 14:41:54 -0300

    fazendo quadrado de *

---
+++ fundamentos/bloco4-JavaScript/bonus.js
@@ -0,0 +1,10 @@
+let n = 4
+let simbolo = "*"
+let linha =""
+
+for (let index = 0; index < n ; index += 1){
+   linha = linha + simbolo
+}
+for(let index =0; index < n ;index += 1){
+   console.log (linha)
+}
