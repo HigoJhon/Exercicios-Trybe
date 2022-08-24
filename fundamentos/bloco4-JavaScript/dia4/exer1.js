4404a20ed9176003b1dfe4920228406a77c1931d
Higo Jhon <higojhon98@gmail.com>
2022-08-24 13:51:36 -0300

    criando console.log do leitor

--- fundamentos/bloco4-JavaScript/dia4/exer1.js
+++ fundamentos/bloco4-JavaScript/dia4/exer1.js
@@ -5,7 +5,32 @@ let info = {
     
   };
   console.log(`Bem-vinda, ${info.personagem}`)
+// parte 2
+
 
   info.recorrente ="sim";
 
   console.log (info)
+// parte 3
+
+
+  for(let index in info){
+    console.log (index)
+  }
+// parte 4 
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: 
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  
+};
+
+console.log (`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama ${leitor.livrosFavoritos.titulo}`)
+
