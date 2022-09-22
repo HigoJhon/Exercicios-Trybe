a9dc1dcdf41004bac6ec0f351d91d4765a60683a
Higo Jhon <higojhon98@gmail.com>
2022-09-22 19:21:10 -0300

    usando callBack

--- fundamentos/secao8/dia1/script.js
+++ fundamentos/secao8/dia1/script.js
@@ -1,16 +1,16 @@
-const newEmployees = (chamar) => {
+const newEmployees = (callBack) => {
     const employees = {
-        pessoa1: chamar('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-        pessoa2: chamar('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-        pessoa3: chamar('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+        pessoa1: callBack('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+        pessoa2: callBack('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+        pessoa3: callBack('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
     }
     return employees;
 };
 
-const criarEmail = (criar) => {
+const callBack = (criar) => {
 
     const email = criar.replace(' ', '_');
     return `${email}@betrybe.com`;
 }
 
-console.log(newEmployees(criarEmail))
+console.log(newEmployees(callBack))
